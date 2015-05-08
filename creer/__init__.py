05f64b5a2c816bb7597414a838ea3bc285bb3f0b
Jacob Fischer <jacob.t.fischer@gmail.com>
2015-05-07 22:23:00 -0500

    by default outputs to new output folder

--- creer/__init__.py
+++ creer/__init__.py
@@ -3,11 +3,11 @@ import creer.prototype as prototype
 import creer.template as template
 import creer.writer as writer
 
-def run(main, input, output, clean=False):
+def run(main, input, output, merge=False):
     datas = data.parse(main)
 
     proto = prototype.build(datas)
 
-    generated_files = template.build_all(proto, input, output, clean)
+    generated_files = template.build_all(proto, input, output, merge)
 
     writer.write(generated_files)
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ parser = argparse.ArgumentParser(description='Runs the Creer game generator with
 parser.add_argument('-g, --game', action='store', dest='game', default='./main.data', help='the file that should be treated as the main data file for game generation')
 parser.add_argument('-o, --output', action='store', dest='output', help='the path to the folder to put generated folders and files into. If omitted it will output and overwrite the input files')
 parser.add_argument('-i, --input', action='store', dest='input', nargs='+', help='the path(s) to look for templates in "_templates/" to build output from. can be a list of inputs seperated via spaces. defaults to all the siblings directories with creer templates.')
-parser.add_argument('--clean', action='store_true', dest='clean', default=False, help='if the output files should NOT be merged with existing files')
+parser.add_argument('--merge', action='store_true', dest='merge', default=False, help='if the output files should be merged with existing files')
 
 args = parser.parse_args()
 
-creer.run(args.game, args.input, args.output, args.clean)
+creer.run(args.game, args.input, args.output, args.merge)
