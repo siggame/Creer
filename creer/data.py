e14421ba66eaab439757090ed9b882fda2f0d623
Jacob Fischer <jacob.t.fischer@gmail.com>
2017-02-18 09:24:57 -0600

    Add YAML support

--- creer/data.py
+++ creer/data.py
@@ -1,33 +1,47 @@
 import os
 import json
+import yaml
 import fnmatch
 
+_parser = {
+    'json': json,
+    'data': json,
+    'yaml': yaml
+}
+
 def _parse_data(datas, key, path):
+    filename, ext = os.path.splitext(path)
+
     try:
-        f = open(path)
         print("-- PARSING", path)
-        data = json.load(f)
-        f.close()
+        with open(path, 'r') as file:
+            data = _parser[ext[1:].lower()].load(file)
     except ValueError as e:
         print("Error reading data file '" + path + "' -", e)
         raise Exception("Error reading data file")
     else:
         datas[key] = data
 
+
 def parse(main_path):
     datas = {}
-    if not os.path.isfile(main_path):
-        generic_path = "../Games/" + main_path + "/creer.data"
-        if os.path.isfile(generic_path):
-            main_path = generic_path
-        else:
-            raise Exception("main.data path ("+ main_path + ") not valid as generic path or actual path.")
+    found = os.path.isfile(main_path)
+    if not found:
+        for extension in _parser.keys():
+            generic_path = '../Games/{}/creer.{}'.format(main_path, extension)
+            found = os.path.isfile(generic_path)
+            if found:
+                main_path = generic_path
+                break
+
+    if not found:
+        raise Exception("main.data path (" + main_path + ") not valid as generic path or actual path.")
 
     _parse_data(datas, "main", main_path)
 
-    extension = ".data"
-    for root, dirnames, filenames in os.walk('datas'):
-        for filename in fnmatch.filter(filenames, "*" + extension):
-            _parse_data(datas, os.path.splitext(filename)[0], os.path.join(root, filename))
+    for extension in _parser.keys():
+        for root, dirnames, filenames in os.walk('datas'):
+            for filename in fnmatch.filter(filenames, "*" + extension):
+                _parse_data(datas, os.path.splitext(filename)[0], os.path.join(root, filename))
 
     return datas
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import creer
 
 parser = argparse.ArgumentParser(description='Runs the Creer game generator with a main data file against imput templates to generate an output skeleton game framework')
-parser.add_argument('game', action='store', help='the file that should be treated as the main data file for game generation')
+parser.add_argument('game', action='store', help='the file that should be treated as the main data file/folder for game generation. Must be json or yaml')
 parser.add_argument('-o, --output', action='store', dest='output', help='the path to the folder to put generated folders and files into. If omitted it will output and overwrite the input files')
 parser.add_argument('-i, --input', action='store', dest='inputs', nargs='+', help='the path(s) to look for templates in "_templates/" to build output from. can be a list of inputs seperated via spaces. defaults to all the siblings directories with creer templates.')
 parser.add_argument('--merge', action='store_true', dest='merge', default=False, help='if the output files should be merged with existing files')
