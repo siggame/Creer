0c92661cb7629ee2be1c45897620a83362c70ff0
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-10-01 18:18:27 -0500

    Add checking for empty templates on input directories

--- creer/input.py
+++ creer/input.py
@@ -1,4 +1,6 @@
 import glob
+from os import path
+from creer.template import templates_folder
 
 def validate(inputs):
     validated_inputs = []
@@ -6,6 +8,9 @@ def validate(inputs):
         dirs = glob.glob(input_dir)
         if not dirs:
             raise Exception("No directories matching {}".format(input_dir))
+
+        if not glob.glob(path.join(input_dir, templates_folder)):
+            raise Exception("Cannot template a directory with no Creer templates!\nNo template directory '{}' in {}".format(templates_folder, input_dir))
         validated_inputs.extend(dirs)
 
     for validated_input in validated_inputs:
